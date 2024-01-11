dec17369b890be00d8854e426e07ea8b139eb708
IonutInit <ionutcojocaru@outlook.com>
2024-01-11 16:46:43 +0000

    added basic hmtl

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"lens-locked/controllers"
+	"lens-locked/templates"
+	"lens-locked/views"
+	"net/http"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", controllers.StaticHandler(views.Must(
+		views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
+
+	r.Get("/contact", controllers.StaticHandler(views.Must(
+		views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
+
+	r.Get("/faq", controllers.FAQ(
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
+
+	//r.Get("/signup", controllers.StaticHandler(views.Must(
+	//	views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))))
+
+	var usersC controllers.Users
+	usersC.Templates.New = views.Must(views.ParseFS(
+		templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	r.Get("/signup", usersC.New)
+	r.Post("/signup", usersC.Create)
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+	fmt.Println("Starting server on port :3000...")
+	http.ListenAndServe(":3000", r)
+}
---
+++ views/template.go
@@ -0,0 +1,50 @@
+package views
+
+import (
+	"fmt"
+	"html/template"
+	"io/fs"
+	"log"
+	"net/http"
+)
+
+func (t Template) Execute(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err := t.htmlTpl.Execute(w, data)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "Error executing the template", http.StatusInternalServerError)
+		return
+	}
+}
+
+type Template struct {
+	htmlTpl *template.Template
+}
+
+func ParseFS(fs fs.FS, pattern ...string) (Template, error) {
+	htmlTpl, err := template.ParseFS(fs, pattern...)
+	if err != nil {
+		return Template{}, fmt.Errorf("parsing template: %w", err)
+	}
+	return Template{
+		htmlTpl: htmlTpl,
+	}, nil
+}
+
+//func Parse(filepath string) (Template, error) {
+//	htmlTpl, err := template.ParseFiles(filepath)
+//	if err != nil {
+//		return Template{}, fmt.Errorf("parsing template: %w", err)
+//	}
+//	return Template{
+//		htmlTpl: htmlTpl,
+//	}, nil
+//}
+
+func Must(t Template, err error) Template {
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
